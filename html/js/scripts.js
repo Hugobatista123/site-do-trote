2208fd00f23455aa9e0e7c15ab338f8ed476ea60
Hugobatista123 <hugo.batista.aguiar@gmail.com>
2025-03-31 20:30:07 -0300

    sitedotrote

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,15 +1,31 @@
 function calcular (){
-    let acaoSocial = document.getElementById("acaoSocial").value
+    let acaoSocial = Number(document.getElementById("acaoSocial").value)
 
-    let homenagem = document.getElementById ("homenagem").value
+    let homenagem = Number(document.getElementById ("homenagem").value)
 
-    let leite = document.getElementById("leite").value
+    let leite = Number(document.getElementById("leite").value)
 
-    let kitavulso = document.getElementById("kitavulso").value
+    let kit = Number(document.getElementById("kit").value)
+
+    let equipe = (document.getElementById("equipe").value)
+    let pontoskit = 0
+    if (equipe == "Laranja"){
+      if (Number (kit) >= 97){
+        pontoskit = 5000 +((kit - 97) * 30)
+         if  ((kit) >= 78){
+          pontoskit = 
+        }
+      }
+    }
+    let preta = (document.getElementById("preta").value)
+    let pontoskit2 = 0
+    if (equipe == "Preta"){
+      if (kit) >=
+    }
 
     let latadesup = document.getElementById("latadesup").value
 
-    let soma = Number(acaoSocial) + Number(homenagem) + ( 2 * leite) + (30 * kitavulso) + (15 * latadesup)
+    let soma = Number(acaoSocial) + Number(homenagem) + ( 2 * leite) + (30 * kit) + (15 * latadesup)
 
     document.getElementById("soma").innerHTML = soma.toFixed(2)
   }
